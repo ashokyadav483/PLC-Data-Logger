980c9dce56f50e5cbdf0ce5822add1718e6a5c0b
ashokyadav483 <ashok.yadav483@gmail.com>
2026-03-22 17:10:52 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# OPC UA Server URL (change as per your PLC)
+OPC_URL = "opc.tcp://192.168.0.2:4840"
+
+# Node ID (change based on your PLC tag)
+NODE_ID = "ns=4;i=2"
+
+# Scan time in seconds
+SCAN_TIME = 2
